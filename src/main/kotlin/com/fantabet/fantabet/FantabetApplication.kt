bd43d4207b0177db78eb7b36eb5d1027cd3792da
Angelo <angelo.adduci1991@gmail.com>
2026-03-11 19:47:24 +0100

    Init spring boot kotlin project

---
+++ src/main/kotlin/com/fantabet/fantabet/FantabetApplication.kt
@@ -0,0 +1,11 @@
+package com.fantabet.fantabet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FantabetApplication
+
+fun main(args: Array<String>) {
+	runApplication<FantabetApplication>(*args)
+}
---
+++ src/test/kotlin/com/fantabet/fantabet/FantabetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fantabet.fantabet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FantabetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
